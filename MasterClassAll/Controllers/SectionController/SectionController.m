c1ea59875fe09363c3224f6cf4fb41cb2a2450ee
Carl <zeng1090684394@163.com>
2020-10-17 13:47:20 +0800

    Changed

--- MasterClassAll/Controllers/SectionController/SectionController.m
+++ MasterClassAll/Controllers/SectionController/SectionController.m
@@ -29,10 +29,14 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     TitleCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TitleCell" forIndexPath:indexPath];
     
-    NSString *title = [NSString stringWithFormat:@"Section%li", indexPath.row + 1];
+    NSString *sectionIndex = [NSString stringWithFormat:@"Section%li", indexPath.row + 1];
+    NSString *theme = [self getTitleNameWithIndex:indexPath.row];
+    NSString *title = [NSString stringWithFormat: @"%@  [%@]", sectionIndex, theme];
+    
     cell.titleColor = [UIColor purpleColor];
-    cell.font = [UIFont fontWithName:@"Arial Bold" size:20];
+    cell.font = [UIFont fontWithName:@"Arial Bold" size:18];
     cell.customBackgroundColor = [UIColor systemGray5Color];
+    
     cell.titleStr = title;
     
     return cell;
@@ -45,23 +49,29 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    NSString *className = [self.controllers objectAtIndex:indexPath.row];
+    NSString *className = [self getTitleNameWithIndex:indexPath.row];
     UIViewController *controller = [self createControllerWithString:className];
-    NSString *title;
-    if (className.length > 2) {
-        title = [className substringToIndex:[className length] - 2];
-    } else {
-        title = @"Theme";
-    }
-    controller.navigationItem.title = title;
     
     [self.navigationController pushViewController:controller animated:YES];
     
 }
 
+- (NSString *)getTitleNameWithIndex: (NSInteger)index {
+    NSString *title = [self.controllers objectAtIndex: index];
+    return title;
+}
+
 - (UIViewController *)createControllerWithString: (NSString *)name {
     NSLog(@"%@", name);
-    id theClass = [[NSClassFromString(name) alloc] init];
+    UIViewController* theClass = [[NSClassFromString(name) alloc] init];
+    
+    NSString *title;
+    if (name.length > 2) {
+        title = [name substringToIndex:[name length] - 2];
+    } else {
+        title = @"Theme";
+    }
+    theClass.navigationItem.title = title;
     
     return theClass;
 }
